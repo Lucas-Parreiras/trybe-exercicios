232bbf44601027b2bd9fca14a791516c12d4e5dd
Lucas-Parreiras <lucassouzaparreiras@hotmail.com>
2022-09-22 14:35:29 -0300

    "commit perdido no vscode"

---
+++ fundamentos-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos-javascript/scripts8.js
@@ -0,0 +1,16 @@
+const c = 23;
+const d = 65;
+
+if (c > d) {
+    console.log(c)
+}
+
+else if (d > c) {
+    console.log(d)
+}
+
+else {
+    console.log("Os números são íguais.")
+}
+
+// Tudo certo.
