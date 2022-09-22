556ca5b475936b9cd35e147faff98f939808fb6d
Lucas-Parreiras <lucassouzaparreiras@hotmail.com>
2022-09-22 14:21:42 -0300

    [Lucas-Parreiras]Adiciona exercícios dia 4.2

---
+++ fundamentos-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-arrays-e-for/scripts.js
@@ -0,0 +1,60 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+// Tudo certo!
+
+// let somaTotal = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     somaTotal = somaTotal + numbers[index];
+// }
+// console.log(somaTotal);
+
+// let mediaAritmetica = somaTotal / numbers.length;
+
+// console.log(mediaAritmetica);
+
+// if (mediaAritmetica > 20) {
+//     console.log("Valor maior que 20!");
+// }
+// else {
+//     console.log("Valor menor que 20!");
+// }
+// Tudo certo!
+
+// let maior = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > maior) {
+//         maior = numbers[index];
+//     }
+// }
+// console.log(maior);
+// Tudo ok!
+
+// let impares = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 !== 0) {
+//         impares += 1;
+//     }
+// }
+// if (impares > 0) {
+//     console.log(impares);
+// }
+// else {
+//     console.log("Nenhum valor ímpar encontrado.");
+// }
+// Tudo ok!
+
+// let menor = 1000;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] < menor) {
+//         menor = numbers[index];
+//     }
+// }
+// console.log(menor);
+// Tudo certo!
+
+
+
