57c21843e51bc1a16d7455f157def996b3a18f0d
Lucas-Parreiras <lucassouzaparreiras@hotmail.com>
2022-09-29 19:11:54 -0300

    [Lucas-Parreiras] Completado exercício 1

--- fundamentos-desenvolvimento-web/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos-desenvolvimento-web/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -2,8 +2,14 @@ function headerBackgroundColor(id, newColor) {
     document.getElementById(id).style.backgroundColor = newColor;
 }
 headerBackgroundColor("header-container", "green");
+headerBackgroundColor("footer-container", "darkgreen");
 
-function taskBackColor(param1, newBackColor) {
-    document.getElementsByTagName(param1).style.backgroundColor = newBackColor;
+function taskBackColor(param1, newBackColor, index) {
+    document.getElementsByClassName(param1)[index].style.backgroundColor = newBackColor;
 }
-taskBackColor("section", "orange");
+taskBackColor("emergency-tasks", "orange", "0");
+taskBackColor("no-emergency-tasks", "yellow", "0");
+taskBackColor("title-emergency", "purple", "0");
+taskBackColor("title-emergency", "purple", "1");
+taskBackColor("title-no-emergency", "black", "0");
+taskBackColor("title-no-emergency", "black", "1");
