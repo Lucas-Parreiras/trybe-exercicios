4a4c9f317d526b903a58e92410424be13c030b6f
Lucas Parreiras <lucassouzaparreiras@hotmail.com>
2022-11-22 16:40:00 -0300

    Primeiro passos em react.

---
+++ front-end/secao-02/dia-01/first-react-app/src/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Lucas Parreiras
+        </h1>
+        <p>
+          Sobre mim.
+        </p>
+        <h2>
+          Minhas Habilidades:
+        </h2>
+        <ul>
+          <li>Hab 1.</li>
+          <li>Hab 2.</li>
+          <li>Hab 3.</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default About;
