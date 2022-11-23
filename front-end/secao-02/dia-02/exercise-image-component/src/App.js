5a42aa2b7a81dca545385648b3f5d1789b81e606
Lucas Parreiras <lucassouzaparreiras@hotmail.com>
2022-11-23 15:40:01 -0300

    exercicio 1 feito

--- front-end/secao-02/dia-02/exercise-image-component/src/App.js
+++ front-end/secao-02/dia-02/exercise-image-component/src/App.js
@@ -1,9 +1,10 @@
 import Image from './Image';
+import catStaring from './catStaring.jpg';
 
 function App() {
   return (
     <div>
-      <Image src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Cute cat staring" />
+      <Image source={ catStaring } alternativeText="Cute cat staring" />
     </div>
   );
 }
